d1c9ce9d918dca709f70f948b3baaa186f9b0c57
Musa Dzeytov <musa.dzeytov@student.kdg.be>
2025-01-05 22:37:17 +0100

    updates

--- src/components/activities/overview/ActivitySpeedDial.tsx
+++ src/components/activities/overview/ActivitySpeedDial.tsx
@@ -1,54 +1,61 @@
-import React, {useState} from 'react';
-import {Box, ClickAwayListener, SpeedDial, SpeedDialAction, SpeedDialIcon, useMediaQuery} from '@mui/material';
-import PeopleIcon from '@mui/icons-material/People';
-import ConnectWithoutContactIcon from '@mui/icons-material/ConnectWithoutContact';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import {useNavigate} from 'react-router-dom';
-import {useTranslation} from 'react-i18next';
-import {SpeedDialDirectionsEnum} from '../../../types/enums/ComponentPropsEnums';
-import {ActivityTypesEnum} from '../../../types/enums/ActivityTypesEnum';
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import PeopleIcon from "@mui/icons-material/People";
+import {
+  Box,
+  ClickAwayListener,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  useMediaQuery,
+} from "@mui/material";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { ActivityTypesEnum } from "../../../types/enums/ActivityTypesEnum";
+import { SpeedDialDirectionsEnum } from "../../../types/enums/ComponentPropsEnums";
 
-interface Props {
-	direction: SpeedDialDirectionsEnum | undefined;
-}
+type TProps = {
+  direction: SpeedDialDirectionsEnum | undefined;
+};
 
 const actions = [
-	{icon: <PeopleIcon/>, name: ActivityTypesEnum.Physical},
-	{icon: <ConnectWithoutContactIcon/>, name: ActivityTypesEnum.Online},
-	{icon: <LocalPhoneIcon/>, name: ActivityTypesEnum.Phone},
+  { icon: <PeopleIcon />, name: ActivityTypesEnum.Physical },
+  { icon: <ConnectWithoutContactIcon />, name: ActivityTypesEnum.Online },
+  { icon: <LocalPhoneIcon />, name: ActivityTypesEnum.Phone },
 ];
 
-export function ActivitySpeedDial({direction}: Props) {
-	const [open, setOpen] = useState(false);
-	const navigate = useNavigate();
-	const {t} = useTranslation();
-	const isMobile = useMediaQuery('(max-width: 600px)');
+const ActivitySpeedDial = ({ direction }: TProps) => {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
-	const handleClick = () => setOpen(!open);
+  return (
+    <Box sx={{ transform: "translateZ(0)", flexGrow: 1 }}>
+      <ClickAwayListener onClickAway={() => setOpen(false)}>
+        <SpeedDial
+          ariaLabel="Activity speed dial"
+          sx={{ position: "absolute", bottom: 6, right: 6 }}
+          icon={<SpeedDialIcon />}
+          direction={direction}
+          onClick={() => setOpen(!open)}
+          open={open}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={
+                !isMobile ? t(`Common.Activity speed dial.${action.name}`) : ""
+              }
+              onClick={() => navigate(`/activities/new/${action.name}`)}
+            />
+          ))}
+        </SpeedDial>
+      </ClickAwayListener>
+    </Box>
+  );
+};
 
-	const handleClickAway = () => setOpen(false);
-
-	return (
-		<Box sx={{transform: 'translateZ(0)', flexGrow: 1}}>
-			<ClickAwayListener onClickAway={handleClickAway}>
-				<SpeedDial
-					ariaLabel='Activity speed dial'
-					sx={{position: 'absolute', bottom: 6, right: 6}}
-					icon={<SpeedDialIcon/>}
-					direction={direction}
-					onClick={handleClick}
-					open={open}
-				>
-					{actions.map((action) => (
-						<SpeedDialAction
-							key={action.name}
-							icon={action.icon}
-							tooltipTitle={!isMobile ? t(`Common.Activity speed dial.${action.name}`) : ''}
-							onClick={() => navigate(`/activities/new/${action.name}`)}
-						/>
-					))}
-				</SpeedDial>
-			</ClickAwayListener>
-		</Box>
-	);
-}
+export default ActivitySpeedDial;
--- src/components/activities/overview/ArchivedActivityCard.tsx
+++ src/components/activities/overview/ArchivedActivityCard.tsx
@@ -1,31 +1,35 @@
-import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
-import React from "react";
-import {IActivity} from "../../../types/models/IActivity";
+import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
 import dayjs from "dayjs";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { IActivity } from "../../../types/models/IActivity";
 
-interface Props {
-	activity: IActivity;
-}
+type TProps = {
+  activity: IActivity;
+};
 
-export default function ArchivedActivityCard({activity}: Props) {
-	const navigate = useNavigate();
+const ArchivedActivityCard = ({ activity }: TProps) => {
+  const navigate = useNavigate();
 
-	return (
-		<Card sx={{width: 210, height: 120}} onClick={() => navigate(`/activities/${activity.guid}`)}>
-			<CardActionArea sx={{width: 210, height: 120}}>
-				<CardContent>
-					<Typography sx={{fontSize: 12}} color="text.secondary" gutterBottom>
-						{activity.type}
-					</Typography>
-					<Typography variant="h6" sx={{fontSize: 14}} component="div" noWrap>
-						{activity.subject}
-					</Typography>
-					<Typography sx={{fontSize: 14}} color="text.secondary">
-						{dayjs(activity.start).format('DD MMM YYYY HH:mm')}
-					</Typography>
-				</CardContent>
-			</CardActionArea>
-		</Card>
-	);
-}
+  return (
+    <Card
+      sx={{ width: 210, height: 120 }}
+      onClick={() => navigate(`/activities/${activity.id}`)}
+    >
+      <CardActionArea sx={{ width: 210, height: 120 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
+            {activity.type}
+          </Typography>
+          <Typography variant="h6" sx={{ fontSize: 14 }} component="div" noWrap>
+            {activity.subject}
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary">
+            {dayjs(activity.startTime).format("DD MMM YYYY HH:mm")}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default ArchivedActivityCard;
